8a8027c97ffaad5c309ada78d77b3d2ee7c85bd0
igor smirnov <smirnoviv@rambler.ru>
2018-04-27 19:23:47 +0500

    Add file exploring

--- udp-launch.py
+++ udp-launch.py
@@ -6,12 +6,13 @@ from threading import Thread
 import subprocess
 import time
 from argparse import ArgumentParser
-from os import listdir
+from os import listdir, stat, remove
 from os.path import isfile, join
 
 parser = ArgumentParser(description='UDP service-launcher')
 parser.add_argument('-p', type=int, default=5007, help='Set listen port')
 parser.add_argument('-d', type=str, default='/home/igor/launch/', help='Directory with .launch files')
+parser.add_argument('-b', type=str, default='/home/igor/bag/', help='Directory with .bag files')
 
 args = parser.parse_args()
 
@@ -20,6 +21,7 @@ launch_threads = {}
 
 UDP_port = args.p
 launchDir = args.d
+bagDir = args.b
 
 running = True
 
@@ -50,14 +52,44 @@ def launch(args, ip, port, id):
         tx_sock.close()
         launch_threads.pop(id, None)
 
+class FileListClient:
+    def __init__(self, id, folder, period, address_port):
+        if folder == "bag":
+            folder = bagDir
+        self.id = id
+        self.folder = folder
+        self.period = period
+        self.counter = period
+        self.address_port = address_port
+    def list(self):
+        p = self.folder
+        return [f + ":" + str(stat(join(p, f)).st_size) for f in listdir(p) if (f.endswith(".bag") or f.endswith(".bag.active")) and isfile(join(p, f))]
+    def tick(self, txSock):
+        self.counter -= 1
+        if self.counter == 0:
+            self.counter = self.period
+            txSock.sendto("@" + self.id + " " + " ".join(self.list()), self.address_port)
 
-def listen(listen_ip, udp_port):
+def remove_files(folder, name):
+    if folder == "bag":
+        folder = bagDir
+    if name.startswith("*"):
+        ext = name[1:]
+        for f in listdir(folder):
+            fn = join(folder, f)
+            if f.endswith(ext) and isfile(fn):
+                remove(fn)
+    else:
+        remove(join(folder, name))
+
+def listen(listen_ip, listen_port):
+    files_clients = {}
     rxSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     txSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
     try:
-        print "Start listen at " + listen_ip + ":" + str(udp_port)
+        print "Start listen at " + listen_ip + ":" + str(listen_port)
         rxSock.settimeout(1)
-        rxSock.bind((listen_ip, udp_port))
+        rxSock.bind((listen_ip, listen_port))
 
         while running:
             try:
@@ -105,11 +137,22 @@ def listen(listen_ip, udp_port):
                             answer("\nKILL " + id + " OK")
                     if cmd == 'tasks':
                         answer("\nTASKS " + task_list())
+                    if cmd == 'files':
+                        id = data[2]
+                        folder = data[3]
+                        period = int(data[4])
+                        files_clients.update({address + ":" + str(tx_port) + ":" + folder : FileListClient(id, folder, period, (address, tx_port))})
+                    if cmd == 'remove':
+                        folder = data[2]
+                        name = data[3]
+                        remove_files(folder, name)
                 except Exception as e:
                     print "Process exception: ", e
                     answer("\nEXCEPTION " + cmd + " " + str(e))
             except socket.timeout:
                 pass
+            for key in files_clients:
+                files_clients[key].tick(txSock)
     except Exception as e:
         print "Loop exception: ", e
     finally:
